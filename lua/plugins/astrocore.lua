f038d79673b67a989caf28fbf9cccfab2f1a065e
Jiri Popek <jiri.popek@bmw.de>
2024-11-28 17:49:53 +0100

    dashboard tweeks

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -66,6 +66,18 @@ return {
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+        ["<Leader>c"] = {
+          function()
+            local bufs = vim.fn.getbufinfo({ buflisted = true })
+            require("astrocore.buffer").close(0)
+            if
+              require("astrocore").is_available("alpha-nvim") and not bufs[2]
+            then
+              require("alpha").start()
+            end
+          end,
+          desc = "Close buffer",
+        },
       },
     },
   },
